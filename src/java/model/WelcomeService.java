bac7b4560eee72a2f4c919b3c6dbcc3afcd25dcf
mcendrowski <mcendrowski@QG273317.qg.com>
2016-01-27 13:39:46 -0600

    first version for lab4

---
+++ src/java/model/WelcomeService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author mcendrowski
+ */
+public class WelcomeService {
+    private LocalDateTime currentDate;
+//    date = 
+    // method for morning, afternoon and evening returning a string
+    // method that takes person's name and returns name and the date result:
+    // "Good afternoon, Mariusz. Welcome!"
+    
+//    <form id=”form1” name=”form1” method=”POST”
+//action=”greeter”>
+    
+    
+}
